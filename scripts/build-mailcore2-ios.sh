734db1ffc3a55ef09bd0944530de052c3f787b9c
Alex Severinov <severinov_a@ukr.net>
2018-08-02 16:36:35 +0300

    set rev for `mailcore2-framework`

--- scripts/build-mailcore2-ios.sh
+++ scripts/build-mailcore2-ios.sh
@@ -7,7 +7,7 @@ popd > /dev/null
 . "$scriptpath/include.sh/build-dep.sh"
 
 url="https://github.com/AlexSeverinov/mailcore2.git"
-rev=f820683afa0a05c1f788157c27e4194a07cf7dc2
+rev=980b2b6b9a2d16fe84d9da35101ddfa13f9f9ed7
 name="mailcore2-ios"
 xcode_target="static mailcore2 ios"
 xcode_project="mailcore2.xcodeproj"
